3e0e69fe7fdec20280b9296addeb468e650d33dd
fyvivas <fyvivas@gmail.com>
2021-04-26 12:35:02 -0500

    create testgpio

---
+++ TESTGPIO/TestGpio.X/pinout.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   pinout.h
+ * Author: ASUS
+ *
+ * Created on 26 de abril de 2021, 12:32 PM
+ */
+
+#ifndef PINOUT_H
+#define	PINOUT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PINOUT_H */
+
